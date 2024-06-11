9d658221f9fc09a0406b631a0a361c6e12010347
tommy-pellerin <tommytp@hotmail.fr>
2024-06-11 14:38:24 +0200

    home page ok

--- src/components/home/MultiCard.jsx
+++ src/components/home/MultiCard.jsx
@@ -3,7 +3,7 @@ import { Grid } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import { Button, CardActionArea, CardActions } from "@mui/material";
+import { Container, Button, CardActionArea, CardActions } from "@mui/material";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { getData } from "../service/apiManager.js";
 
@@ -24,37 +24,39 @@ const CardGrid = () => {
   // ... vos données de carte
 
   return (
-    <Grid container spacing={4}>
-      {products.map((product) => (
-        <Grid item key={product.id} xs={12} sm={6} md={4}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea component={Link} to={`/product/${product.id}`}>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {product.title} {product.price}€
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {product.description}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button
-                size="small"
-                color="primary"
-                component={Link}
-                to={`/product/${product.id}`}
-              >
-                Voir les détails
-              </Button>
-              <Button size="small" color="primary">
-                Share
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
+    <Container component="main" maxWidth="lg">
+      <Grid container spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4}>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea component={Link} to={`/product/${product.id}`}>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {product.title} {product.price}€
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {product.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button
+                  size="small"
+                  color="primary"
+                  component={Link}
+                  to={`/product/${product.id}`}
+                >
+                  Voir les détails
+                </Button>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
