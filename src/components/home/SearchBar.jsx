1108a9f2cb297de5a65e49b03aabc8799e379c87
tommy-pellerin <tommytp@hotmail.fr>
2024-06-11 18:20:55 +0200

    in progress

--- src/components/home/SearchBar.jsx
+++ src/components/home/SearchBar.jsx
@@ -1,29 +1,48 @@
-import { TextField, InputAdornment, Box } from '@mui/material';
+import { TextField, InputAdornment, Box, FormControl, FormLabel, FormControlLabel, RadioGroup, Radio } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import { useState } from 'react';
 
 function SearchBar() {
   const [city,setCity] = useState("")
+  const [typeOfGood,setTypeOfGood] = useState("")
 
-  const handlesubmit = () => {
+  const handlesubmit = (event) => {
     event.preventDefault();
     console.log(city);
   }
 
   return (
     <Box component="form" onSubmit={handlesubmit}>
-      <TextField
-        variant="outlined"
-        placeholder="Search city"
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          ),
-        }}
-        onChange={(e) => setCity(e.target.value)}
-      />
+      <FormControl>
+        <Box sx={{display:"flex", justifyContent:"center", alignItems:"center"}}>
+          <Box sx={{marginRight: 2}}>
+            <TextField
+              variant="outlined"
+              placeholder="Search city"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </Box>
+          <Box >
+            <FormLabel>Type de logement</FormLabel>
+            <RadioGroup
+              name="type-of-good"
+              value={typeOfGood}
+              onChange={(e)=>setTypeOfGood(e.target.value)}
+            >
+              <FormControlLabel value="Maison" control={<Radio />} label="Maison" sx={{ mb: -2 }} />
+              <FormControlLabel value="Appartement" control={<Radio />} label="Appartement" />
+            </RadioGroup>
+          </Box>
+          
+        </Box>
+      </FormControl>
     </Box>
 
 
