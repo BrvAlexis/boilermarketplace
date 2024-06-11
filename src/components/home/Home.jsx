b408ea4d47d80693438255a982cc2c1b9e10fbe7
tommy-pellerin <tommytp@hotmail.fr>
2024-06-11 16:53:07 +0200

    show map button ok

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,14 +4,24 @@ import { Button, Box } from "@mui/material"
 
 export default function Home() {
   return(
-    <>
-      <Hero />
-      <br />
-      <MultiCard />
-      <div className="showMapButton">
-        <Button>Afficher la carte</Button>
+    <Box sx={{ 
+      display: 'flex', 
+      flexDirection: "column",
+      justifyContent: 'center', 
+      alignItems: 'center',
+    }}>
+      <div>
+        <Hero />
+        <br />
+        <MultiCard />
       </div>
       
-    </>
+        <Button variant="contained" color="secondary" 
+        sx={{ 
+          position: 'sticky', 
+          bottom: "10px",
+        }}>Afficher la carte</Button>
+      
+    </Box>
   )
 }
